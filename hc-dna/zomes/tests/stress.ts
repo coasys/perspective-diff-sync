1b9f384c4f124bf1fda4bfacf1229a1c99a7dad7
Nicolas Luck <nicolas@lucksus.eu>
2022-08-17 15:47:41 +0200

    Set stress iterations to 2

--- hc-dna/zomes/tests/stress.ts
+++ hc-dna/zomes/tests/stress.ts
@@ -28,7 +28,7 @@ export async function stressTest(t) {
     console.log("==============================================")
     console.log("=================START========================")
     console.log("==============================================")
-    for(let i=0; i < 20; i++) {
+    for(let i=0; i < 2; i++) {
         console.log("-------------------------");
         console.log("Iteration: ", i)
         console.log("-------------------------");
