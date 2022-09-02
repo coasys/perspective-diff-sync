7a6655a72e805a0b0f18ee5cc5df34662abef7be
jdeepee <joshuadparkin@gmail.com>
2022-09-02 19:34:26 +0100

    cleanup pull code and remove the addition of diffs to merge entry

--- hc-dna/zomes/perspective_diff_sync/src/pull.rs
+++ hc-dna/zomes/perspective_diff_sync/src/pull.rs
@@ -55,6 +55,7 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
                     }
                 )
             )?;
+            debug!("Merging entries: {:?} and {:?}", latest, current);
             //Create the merge entry
             let hash = create_entry(
                 EntryTypes::PerspectiveDiffEntryReference(
@@ -83,9 +84,8 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
             debug!("completed current search population");
             workspace.print_graph_debug();
         
-        
             //Check if latest diff is a child of current diff
-            let ancestor_status = workspace.get_paths(&latest, &common_ancestor);
+            let ancestor_status = workspace.get_paths(&latest, &current);
             debug!("Ancestor status: {:#?}", ancestor_status);
         
             if ancestor_status.len() > 0 {
@@ -97,69 +97,27 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
                 Ok(fast_forward_squash)
             } else {
                 debug!("Fork detected, attempting merge...");
-                //There is a fork, find all the diffs from a fork and apply in merge with latest and current revisions as parents
-                //Since we used workspace.collect_until_common_ancestor(..) above and sorted afterwards,
-                //the first entry in sorted_diffs must be our common ancestor.
-                //Common ancestor is then used as the starting point of gathering diffs on a fork
-                //search
-                //    .find_common_ancestor(latest_index, current_index)
-                //    .expect("Could not find common ancestor");
-                let fork_paths = workspace.get_paths(&current, &common_ancestor);
+                //Get the paths of latest so we can return to the user in pull
                 let latest_paths = workspace.get_paths(&latest, &common_ancestor);
-                let mut fork_direction: Option<Vec<NodeIndex>> = None;
-        
-                let current_index = workspace.get_node_index(&current).expect("to get index after build_graph()");
+                //Get the common ancestor so we can remove it from the latest paths, since this is already seen by the user pulling
                 let common_ancestor = workspace.get_node_index(&common_ancestor).expect("to get index after build_graph()");
         
                 //debug!("Paths of fork: {:#?}", fork_paths);
                 //debug!("Paths of latest: {:#?}", latest_paths);
                 //debug!("Common ancestor: {:#?}", common_ancestor);
         
-                //Use items in path to recurse from common_ancestor going in direction of fork
-                for path in fork_paths.clone() {
-                    if path.contains(&current_index) {
-                        fork_direction = Some(path);
-                        break;
-                    };
-                }
                 let mut latest_paths = latest_paths.into_iter().flatten().collect::<Vec<_>>();
                 latest_paths = dedup(&latest_paths);
                 latest_paths.retain(|val| val != common_ancestor);
         
-                //Create the merge entry
-                let mut merge_entry = PerspectiveDiff {
+                debug!(
+                   "Will merge entries: {:#?} and {:#?}.",
+                   latest, current
+                );
+                let merge_entry = create_entry(EntryTypes::PerspectiveDiff(PerspectiveDiff {
                     additions: vec![],
-                    removals: vec![],
-                };
-                if let Some(mut diffs) = fork_direction {
-                    diffs.reverse();
-                    diffs.retain(|val| val != common_ancestor);
-                    for diff in diffs {
-                        let hash = workspace.index(diff);
-                        let current_diff = workspace.entry_map.get(&hash).expect("got hash through index above");
-                        let diff_entry = get(current_diff.diff.clone(), GetOptions::latest())?
-                            .ok_or(SocialContextError::InternalError(
-                                "Could not find diff entry for given diff entry reference",
-                            ))?
-                            .entry()
-                            .to_app_option::<PerspectiveDiff>()?
-                            .ok_or(SocialContextError::InternalError(
-                                "Expected element to contain app entry data",
-                            ))?;
-                        merge_entry
-                            .additions
-                            .append(&mut diff_entry.additions.clone());
-                        merge_entry
-                            .removals
-                            .append(&mut diff_entry.removals.clone());
-                    }
-                }
-        
-                //debug!(
-                //    "Will merge entries: {:#?} and {:#?}. With diff data: {:#?}",
-                //    latest, current, merge_entry
-                //);
-                let merge_entry = create_entry(EntryTypes::PerspectiveDiff(merge_entry))?;
+                    removals: vec![]
+                }))?;
                 //Create the merge entry
                 let hash = create_entry(EntryTypes::PerspectiveDiffEntryReference(
                     PerspectiveDiffEntryReference {
@@ -172,8 +130,9 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
                 update_current_revision(hash.clone(), now)?;
                 update_latest_revision(hash, now)?;
         
+                //TODO; this should happen before the merge, so that if the fetching of some diffs fails we dont loose data
                 //Return the diffs unseen by the user
-                let mut unseen_entry = PerspectiveDiff {
+                let mut unseen_diffs = PerspectiveDiff {
                     additions: vec![],
                     removals: vec![],
                 };
@@ -194,10 +153,10 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
                                 .ok_or(SocialContextError::InternalError(
                                     "Expected element to contain app entry data",
                                 ))?;
-                            unseen_entry
+                            unseen_diffs
                                 .additions
                                 .append(&mut diff_entry.additions.clone());
-                            unseen_entry
+                            unseen_diffs
                                 .removals
                                 .append(&mut diff_entry.removals.clone());
                         }
@@ -211,15 +170,15 @@ pub fn pull() -> SocialContextResult<PerspectiveDiff> {
                             .ok_or(SocialContextError::InternalError(
                                 "Expected element to contain app entry data",
                             ))?;
-                        unseen_entry
+                        unseen_diffs
                             .additions
                             .append(&mut diff_entry.additions.clone());
-                        unseen_entry
+                        unseen_diffs
                             .removals
                             .append(&mut diff_entry.removals.clone());
                     }
                 }
-                Ok(unseen_entry)
+                Ok(unseen_diffs)
             }
         }
     }
