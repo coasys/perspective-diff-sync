7de01a674b17523e676abe945464f5b263bcaf4b
jdeepee <joshuadparkin@gmail.com>
2022-09-08 21:30:50 +0100

    remove prod feature from build script

--- hc-dna/build.sh
+++ hc-dna/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown --features prod
+CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown
 hc dna pack workdir
