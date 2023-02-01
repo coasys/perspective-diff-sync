65e41852101b49fb41ff857a1590ab1b4c063319
Joshua Parkin <joshuadparkin@gmail.com>
2023-02-01 16:34:13 +0000

    fix signal handler test

--- hc-dna/zomes/tests/signals.ts
+++ hc-dna/zomes/tests/signals.ts
@@ -9,41 +9,48 @@ export async function signals(t) {
     let aliceSignalCount = 0;
     let bobSignalCount = 0;
     
-    const aliceHapps = await scenario.addPlayerWithApp({
-        bundle: {
-            manifest: {
-                manifest_version: "1",
-                name: "perspective-diff-sync",
-                roles: [{
-                    name: "main",
-                    dna: {
-                        //@ts-ignore
-                        path: resolve(dnas[0].source.path)
-                    }
-                }]
+    const aliceHapps = await scenario.addPlayerWithApp(
+        {
+            bundle: {
+                manifest: {
+                    manifest_version: "1",
+                    name: "perspective-diff-sync",
+                    roles: [{
+                        name: "main",
+                        dna: {
+                            //@ts-ignore
+                            path: resolve(dnas[0].source.path)
+                        }
+                    }]
+                },
+                resources: {}
             },
-            resources: {}
-        }, 
-        signalHandler: (signal) => {
-            console.log("Alice Received Signal:",signal)
-            aliceSignalCount += 1;
+        },
+        {
+            signalHandler: (signal) => {
+                console.log("Alice Received Signal:",signal)
+                aliceSignalCount += 1;
+            }
         }
-    });
-    const bobHapps = await scenario.addPlayerWithApp({
-        bundle: {
-            manifest: {
-                manifest_version: "1",
-                name: "perspective-diff-sync",
-                roles: [{
-                    name: "main",
-                    dna: {
-                        //@ts-ignore
-                        path: resolve(dnas[0].source.path)
-                    }
-                }]
-            },
-            resources: {}
+    );
+    const bobHapps = await scenario.addPlayerWithApp(
+        {
+            bundle: {
+                manifest: {
+                    manifest_version: "1",
+                    name: "perspective-diff-sync",
+                    roles: [{
+                        name: "main",
+                        dna: {
+                            //@ts-ignore
+                            path: resolve(dnas[0].source.path)
+                        }
+                    }]
+                },
+                resources: {}
+            }
         }, 
+        {
         signalHandler: (signal) => {
             console.log("Bob Received Signal:",signal)
             bobSignalCount += 1;
