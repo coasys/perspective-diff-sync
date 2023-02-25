46de4c71c7bacc6d4c2083c33cd0774250b833f0
Nicolas Luck <nicolas@lucksus.eu>
2023-02-25 13:39:34 +0100

    Log out hash of current revision when it's retrieved

--- hc-dna/zomes/perspective_diff_sync/src/link_adapter/revisions.rs
+++ hc-dna/zomes/perspective_diff_sync/src/link_adapter/revisions.rs
@@ -36,10 +36,11 @@ pub fn latest_revision<Retriever: PerspectiveDiffRetreiver>(
 //Latest revision as seen from our local state
 pub fn current_revision<Retriever: PerspectiveDiffRetreiver>(
 ) -> SocialContextResult<Option<LocalHashReference>> {
-    debug!("===PerspectiveDiffSync.current_revision(): Function start");
-    let now = get_now()?.time();
+    //debug!("===PerspectiveDiffSync.current_revision(): Function start");
+    //let now = get_now()?.time();
     let rev = Retriever::current_revision()?;
-    let after = get_now()?.time();
-    debug!("===PerspectiveDiffSync.current_revision() - Profiling: Took: {} to get the current_revision", (after - now).num_milliseconds());
+    debug!("===PerspectiveDiffSync.current_revision(): rev = {:?}", rev);
+    //let after = get_now()?.time();
+    //debug!("===PerspectiveDiffSync.current_revision() - Profiling: Took: {} to get the current_revision", (after - now).num_milliseconds());
     Ok(rev)
 }
